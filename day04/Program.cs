6d5042395c622bc125b92b32f66caf09c80a414a
Jim Blizzard <jimblizzard@hotmail.com>
2020-01-04 18:25:55 -0500

    d4s2 solution:

--- day04/Program.cs
+++ day04/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace day04
 {
@@ -30,13 +31,16 @@ namespace day04
     {
         private int validPwdCount = 0;
         private char[] potentialPwd;
+        private char matchingChar;
+        private Dictionary<char, int> histogramy;
 
         public int countAllValidPwd(int start, int end)
         {
             for (; start <= end; start++) // we'll always be in range :) 
             {
                 potentialPwd = start.ToString().ToCharArray();
-                if (allGreaterOrEqual() && atLeastOneAdjacent())
+                if (allGreaterOrEqual() && fillHistogramy())
+                // if (allGreaterOrEqual() && atLeastOneAdjacent()) // Part 1 solution
                 {
                     // valid password!
                     validPwdCount++;
@@ -45,15 +49,44 @@ namespace day04
 
             return validPwdCount;
         }
+        
+        public bool fillHistogramy()
+        {
+            histogramy = new Dictionary<char, int>();
+
+            foreach(char key in potentialPwd)
+            {
+                if (!histogramy.TryAdd(key, 1))
+                {
+                    histogramy[key]++;
+                }
+            }
+
+            return atLeastOneDoublet();
+        }
+
+        public bool atLeastOneDoublet()
+        {
+            foreach(KeyValuePair<char, int> gramy in histogramy)
+            {
+                if (gramy.Value == 2)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
 
         // [x] 2. at least 1 adjacent duplicate
         public bool atLeastOneAdjacent()
         {
-            // Console.WriteLine("in atLeastOneAdjacent");
+            // Console.WriteLine("in atLeastOneAdjacent but no more than 2");
             for(int i = 0; i < potentialPwd.Length - 1; i++)
             {   // 1, 0, 0 
-                if (potentialPwd[i] == potentialPwd[i+1])
+                if ((potentialPwd[i] == potentialPwd[i+1])) // && (potentialPwd[i] != matchingChar))
                 {
+                    // matchingChar = potentialPwd[i];
                     return true;
                 }
             }
@@ -64,8 +97,6 @@ namespace day04
         // [x] 3. (n) <= (n+1)
         public bool allGreaterOrEqual()
         {
-            // Console.WriteLine("in allGreaterOrEqual");
-            
             for(int i = 0; i < potentialPwd.Length - 1; i++)
             {   // 1, 0, 0 
                 if ((int)(potentialPwd[i] - '0') > (int)(potentialPwd[i+1] - '0'))
